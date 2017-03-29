a617adf789aab1ae15de09ab364d11e606fd7755
Lluis Montabes <lluis.456@gmail.com>
2017-03-29 18:37:56 +0200

    Implemented send_local_action.php code.

--- send_local_action.php
+++ send_local_action.php
@@ -6,4 +6,22 @@
  * Time: 12:53
  */
 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$con = new mysqli($server, $username, $password, $db);
+
+$json = $_GET['data'];
+$point = json_decode($json);
+
+$x = $point['x'];
+$y = $point['y'];
+
+$result = mysqli_query($con, "UPDATE matches SET player1_x='$x', player1_y='$y' WHERE id='0';")
+
+
 ?>
