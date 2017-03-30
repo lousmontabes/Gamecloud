b97bac4e9a5228ac7017f64aec12a6182633d655
Lluis Montabes <lluis.456@gmail.com>
2017-03-30 16:45:28 +0200

    Començat a implementar principis de find_opponent

--- find_opponent.php
+++ find_opponent.php
@@ -6,6 +6,18 @@
  * Time: 15:53
  */
 
-echo 0;
+$queueId = substr($_GET['id'], 0, -1);
+
+if ($queueId % 2 == 0){
+    // If queue index is odd, actively look for an opponent.
+
+    
+
+}else{
+    // If queue index is even, wait for an opponent to find you.
+
+
+
+}
 
 ?>
