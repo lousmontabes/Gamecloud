fd5a4ff8d1a63af2b5ffb4405c056e80c631d8dc
Lluis Montabes <lluis.456@gmail.com>
2017-03-30 21:16:45 +0200

    Fixing minor errors

--- check_match_requests.php
+++ check_match_requests.php
@@ -23,7 +23,7 @@ if (mysqli_num_rows($result) == 0){
     // There are no active match requests. Create a match request.
 
     $result = mysqli_query($con, "INSERT INTO match_requests(id) VALUE (null)");
-    echo "{\"action\":0,\"id\":0}";
+    echo "{\"action\":0,\"index\":0}";
 
 }else{
 
@@ -32,10 +32,10 @@ if (mysqli_num_rows($result) == 0){
 
     $result = mysqli_query($con, "INSERT INTO matches(id) VALUE (null)");
     $matchId = mysqli_insert_id($con);
-    
+
     $result = mysqli_query($con, "UPDATE match_requests SET assigned_match VALUE {$matchId}");
 
-    echo "{\"action\":1,\"id\":{$matchId}}";
+    echo "{\"action\":1,\"index\":{$matchId}}";
 
 }
 
