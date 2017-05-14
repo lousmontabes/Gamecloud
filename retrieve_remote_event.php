50cc7f8b90053db8b761b6b44aeae26e93d0a513
Lluis Montabes <lluis.456@gmail.com>
2017-05-14 18:44:10 +0200

    Added send_local_event, retrieve_remote_event. Renamed send_local_action and retrieve_remote_action to send_local_position and retrieve_local_position, respectively.

---
+++ retrieve_remote_event.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lluismontabes
+ * Date: 14/5/17
+ * Time: 18:01
+ */
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$con = new mysqli($server, $username, $password, $db);
+
+$matchId = $_GET['matchId'];
+$player = $_GET['player'];
+
+$result = mysqli_query($con, "SELECT player{$player}_event FROM matches WHERE id={$matchId};");
+$row = mysqli_fetch_array($result);
+
+$event = $row[0];
+
+$coordinates = ["event" => $event];
+
+echo json_encode($coordinates);
+
+?>
---
+++ send_local_event.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lluismontabes
+ * Date: 14/5/17
+ * Time: 18:01
+ */
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$con = new mysqli($server, $username, $password, $db);
+
+$matchId = $_GET['matchId'];
+$player = $_GET['player'];
+$event = $_GET['event'];
+
+echo $event;
+
+$result = mysqli_query($con, "UPDATE matches SET player{$player}_event={$event} WHERE id={$matchId};");
+
+?>
