aa92ee226c0fd82309b3fc4cc6c950fb0d3b5dd9
Lluis Montabes <lluis.456@gmail.com>
2017-05-25 12:43:18 +0200

    Testejant implementació modular de la conexió a la base de dades. Implementant funcions de connexió al fitxer connection.php

---
+++ connection.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lluismontabes
+ * Date: 25/5/17
+ * Time: 12:40
+ */
+
+
+function connectToDatabase(){
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
+
+    return new mysqli($server, $username, $password, $db);
+}
+
+?>
