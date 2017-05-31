0aa32f99837f839b17592efd254207092d919c2c
lousmontabes <lluis.456@gmail.com>
2017-05-31 12:44:33 +0200

    Added java client code

---
+++ java_client/Event.java
@@ -0,0 +1,8 @@
+package com.example.lluismontabes.gameofboatsandcards.Gamecloud;
+
+/**
+ * Created by lmontaga7.alumnes on 31/05/17.
+ */
+
+public class Event {
+}
---
+++ java_client/Parameter.java
@@ -0,0 +1,25 @@
+package com.example.lluismontabes.gameofboatsandcards.Gamecloud;
+
+/**
+ * Created by lmontaga7.alumnes on 31/05/17.
+ */
+
+public class Parameter {
+
+    private int index;
+    private String name;
+
+    public Parameter(int index, String name){
+        this.index = index;
+        this.name = name;
+    }
+
+    public int getIndex() {
+        return this.index;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+}
